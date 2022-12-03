39dfd753d2921125d5a45d6632dbf96f18261eb9
ricksterhd123 <ricksterhd123@gmail.com>
2022-12-03 05:24:41 +0000

    POC downloader working

--- mp3-downloader/main.py
+++ mp3-downloader/main.py
@@ -1,4 +1,77 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+from yt_dlp import YoutubeDL
+
+logger = logging.getLogger('mp3-downloader')
+logger.setLevel(logging.INFO)
+
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
+if not BUCKET_NAME:
+    raise Exception("Expected BUCKET_NAME environment variable but got None")
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 def handler(event, context):
-    print("Hello world!")
-    print("Changed!")
-    
+    url = event.get('url')
+
+    if not url:
+        raise Exception("No url provided")
+
+    file_path = '/tmp'
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": f"{file_path}/%(display_id)s.%(ext)s",
+        'postprocessors': [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            },
+            {'key': 'FFmpegMetadata'},
+            {'key': 'EmbedThumbnail'}
+        ],
+        "logger": logger,
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.sanitize_info(ydl.extract_info(url, download=False))
+        video_id = info_dict.get("display_id", None)
+        duration = info_dict.get("duration", None)
+        file_name = f"{video_id}.mp3"
+
+        logger.info(f"Video ID: {video_id}, Duration: {duration}")
+
+        if not duration:
+            logger.warning(f"Video ID: {video_id} has no duration metadata")
+            logger.error("Cannot find duration in metadata. Are you sure this is a YouTube video?")
+            return
+
+        ydl.download([url])
+        upload_file(f"{file_path}/{file_name}", BUCKET_NAME, file_name)
+
+if __name__ == '__main__':
+    handler({
+        "url": "https://www.youtube.com/watch?v=OhILMI3mQ5I"
+    }, {})
--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,27 @@ resource "aws_vpc" "ytmp3-vpc" {
     cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_s3_bucket" "ytmp3-files" {
+    bucket = "ytmp3-files"
+}
+
+resource "aws_s3_bucket_acl" "ytmp3-files-acl" {
+    bucket = aws_s3_bucket.ytmp3-files.id
+    acl    = "private"
+}
+
+resource "aws_iam_policy" "ytmp3-downloader-role-policy" {
+    name = "ytmp3-downloader-role-policy"
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+                Action = "s3:*"
+                Resource = "${aws_s3_bucket.ytmp3-files.arn}/*"
+                Effect = "Allow"
+            }]
+    })
+}
+
 resource "aws_iam_role" "ytmp3-downloader-role" {
     name = "ytmp3-downloader-role"
     assume_role_policy = jsonencode({
@@ -33,7 +54,8 @@ resource "aws_iam_role" "ytmp3-downloader-role" {
     })
 
     managed_policy_arns = [
-        "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+        "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+        aws_iam_policy.ytmp3-downloader-role-policy.arn
     ]
 }
 
@@ -45,4 +67,9 @@ resource "aws_lambda_function" "ytmp3-downloader" {
     memory_size = 512
     timeout = 300
     package_type = "Image"
+    environment {
+      variables = {
+        BUCKET_NAME = aws_s3_bucket.ytmp3-files.id
+      }
+    }
 }
