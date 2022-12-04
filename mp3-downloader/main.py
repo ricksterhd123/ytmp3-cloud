369527c9039f230bcec3443ac146fd1f6d72dffe
ricksterhd123 <ricksterhd123@gmail.com>
2022-12-04 12:52:10 +0000

    Refactor: move utils out of main

--- mp3-downloader/main.py
+++ mp3-downloader/main.py
@@ -1,8 +1,7 @@
 import logging
-import boto3
-from botocore.exceptions import ClientError
 import os
 from yt_dlp import YoutubeDL
+import utils
 
 logger = logging.getLogger('mp3-downloader')
 logger.setLevel(logging.INFO)
@@ -11,27 +10,6 @@ BUCKET_NAME = os.environ.get('BUCKET_NAME')
 if not BUCKET_NAME:
     raise Exception("Expected BUCKET_NAME environment variable but got None")
 
-def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
 
 def handler(event, context):
     url = event.get('url')
@@ -65,11 +43,14 @@ def handler(event, context):
 
         if not duration:
             logger.warning(f"Video ID: {video_id} has no duration metadata")
-            logger.error("Cannot find duration in metadata. Are you sure this is a YouTube video?")
+            logger.error(
+                "Cannot find duration in metadata. Are you sure this is a YouTube video?")
             return
 
         ydl.download([url])
-        upload_file(f"{file_path}/{file_name}", BUCKET_NAME, file_name)
+        utils.s3.upload_file(f"{file_path}/{file_name}",
+                             BUCKET_NAME, file_name)
+
 
 if __name__ == '__main__':
     handler({
