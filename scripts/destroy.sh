619f12749330230ad33b68e167da0fd84b702920
ricksterhd123 <ricksterhd123@gmail.com>
2023-04-21 20:11:53 +0100

    Remove old changes

--- scripts/destroy.sh
+++
@@ -1,4 +0,0 @@
-cd mp3-downloader
-export TF_VAR_mp3_downloader_docker_uri="$(docker inspect --format='{{index .RepoDigests 0}}' ytmp3-downloader-service:latest)"
-cd ../terraform
-terraform destroy
