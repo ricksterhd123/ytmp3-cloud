d7f87f91b2db4c1f3d3578499c892ee6da03f8cd
ricksterhd123 <ricksterhd123@gmail.com>
2023-04-22 02:47:08 +0100

    Update script and deploy script

--- mp3-downloader/main.py
+++ mp3-downloader/main.py
@@ -9,12 +9,9 @@ from botocore.exceptions import ClientError
 logger = logging.getLogger('mp3-downloader')
 logger.setLevel(logging.INFO)
 
-BUCKET_NAME = os.environ.get('BUCKET_NAME')
-if not BUCKET_NAME:
-    raise Exception("Expected BUCKET_NAME environment variable but got None")
-
-logger = logging.getLogger('utils/s3')
-logger.setLevel(logging.INFO)
+YTMP3_STORE_BUCKET_NAME = os.environ.get('YTMP3_STORE_BUCKET_NAME')
+if not YTMP3_STORE_BUCKET_NAME:
+    raise Exception("Expected YTMP3_STORE_BUCKET_NAME environment variable but got None")
 
 def upload_file(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket
@@ -40,10 +37,15 @@ def upload_file(file_name, bucket, object_name=None):
 
 
 def handler(event, context):
-    url = event.get('url')
+    logger.info(event)
+    logger.info(context)
 
-    if not url:
-        raise Exception("No url provided")
+    videoId = event['pathParameters']['videoId']
+
+    if not videoId:
+        return {
+            "statusCode": 400
+        }
 
     file_path = '/tmp'
     ydl_opts = {
@@ -61,26 +63,35 @@ def handler(event, context):
         "logger": logger,
     }
 
-    with YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.sanitize_info(ydl.extract_info(url, download=False))
-        video_id = info_dict.get("display_id", None)
-        duration = info_dict.get("duration", None)
-        file_name = f"{video_id}.mp3"
+    url = f"https://youtube.com/watch?v={videoId}"
 
-        logger.info(f"Video ID: {video_id}, Duration: {duration}")
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.sanitize_info(ydl.extract_info(url, download=False))
+            video_id = info_dict.get("display_id", None)
+            duration = info_dict.get("duration", None)
+            file_name = f"{video_id}.mp3"
 
-        if not duration:
-            logger.warning(f"Video ID: {video_id} has no duration metadata")
-            logger.error(
-                "Cannot find duration in metadata. Are you sure this is a YouTube video?")
-            return
+            logger.info(f"Video ID: {video_id}, Duration: {duration}")
 
-        ydl.download([url])
-        utils.s3.upload_file(f"{file_path}/{file_name}",
-                             BUCKET_NAME, file_name)
+            if not duration:
+                logger.warning(f"Video ID: {video_id} has no duration metadata")
+                logger.error(
+                    "Cannot find duration in metadata. Are you sure this is a YouTube video?")
+                return {
+                    "statusCode": 400,
+                    "body": "Cannot find duration in metadata. Are you sure this is a YouTube video?"
+                }
 
+            ydl.download([url])
+            upload_file(f"{file_path}/{file_name}",
+                                YTMP3_STORE_BUCKET_NAME, file_name)
+    except Exception as e:
+        logger.error(e)
+        return {
+            "statusCode": 500
+        }
 
-if __name__ == '__main__':
-    handler({
-        "url": "https://www.youtube.com/watch?v=OhILMI3mQ5I"
-    }, {})
+    return {
+        "statusCode": 200
+    }
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 set -e
 
 ## This script assumes the following
@@ -10,27 +10,28 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 ECR_REPO_NAME="ytmp3-downloader"
 YTMP3_STORE_BUCKET_NAME="bpmqrjcb"
 
+## FIXME: Can't find a better way to check if repository already exists
 if ! aws ecr create-repository --repository-name $ECR_REPO_NAME; then
-    ## FIXME: Can't find a better way to check if repository already exists,
-    ## I said this script assumes you have the correct permissions...
     echo "ECR repository already exists... I think"
 fi
 
 cd $SCRIPT_DIR/..
-mkdir -p build
-cd build
 
 ## Fetch a static ffmpeg build and put binaries into mp3-downloader/bin for dockerfile
-mkdir -p ffmpeg
-cd ffmpeg
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz.md5
-md5sum -c ffmpeg-git-amd64-static.tar.xz.md5
-tar xvf ffmpeg-git-amd64-static.tar.xz
-mkdir -p ../../mp3-downloader/bin
-mv ffmpeg-git-*-amd64-static/ffmpeg ffmpeg-git-*-amd64-static/ffprobe ../../mp3-downloader/bin
-cd ..
-cd ..
+if [[ ! -d "build/" ]]; then
+    mkdir -p build
+    cd build
+    mkdir -p ffmpeg
+    cd ffmpeg
+    wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+    wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz.md5
+    md5sum -c ffmpeg-git-amd64-static.tar.xz.md5
+    tar xvf ffmpeg-git-amd64-static.tar.xz
+    mkdir -p ../../mp3-downloader/bin
+    mv ffmpeg-git-*-amd64-static/ffmpeg ffmpeg-git-*-amd64-static/ffprobe ../../mp3-downloader/bin
+    cd ..
+    cd ..
+fi
 
 ## Build docker image and push into ecr repository
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.eu-west-2.amazonaws.com
@@ -41,4 +42,4 @@ YTMP3_DOWNLOADER_DOCKER_IMAGE_URI=$(docker inspect --format='{{index .RepoDigest
 
 ## Build the template and deploy guided
 sam build
-sam deploy --guided --parameter-overrides Ytmp3DownloaderDockerImageUri=$YTMP3_DOWNLOADER_DOCKER_IMAGE_URI Ytmp3StoreBucketName=$YTMP3_STORE_BUCKET_NAME
+sam deploy --confirm-changeset --image-repository=$YTMP3_DOWNLOADER_DOCKER_IMAGE_URI --parameter-overrides Ytmp3DownloaderDockerImageUri=$YTMP3_DOWNLOADER_DOCKER_IMAGE_URI Ytmp3StoreBucketName=$YTMP3_STORE_BUCKET_NAME
