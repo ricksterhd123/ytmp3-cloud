6bcdd9b054b9bf667541b2daa31669f80fdd2046
ricksterhd123 <ricksterhd123@gmail.com>
2023-04-23 21:39:20 +0100

    ytmp3-downloader: update status if failed or finished

--- ytmp3-downloader/main.py
+++ ytmp3-downloader/main.py
@@ -5,13 +5,18 @@ import json
 
 from yt_dlp import YoutubeDL
 from botocore.exceptions import ClientError
+from datetime import datetime
 
 logger = logging.getLogger('mp3-downloader')
 logger.setLevel(logging.INFO)
 
+AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION')
 YTMP3_STORE_BUCKET_NAME = os.environ.get('YTMP3_STORE_BUCKET_NAME')
 YTMP3_DOWNLOADER_QUEUE_URL = os.environ.get('YTMP3_DOWNLOADER_QUEUE_URL')
 YTMP3_DB_NAME = os.environ.get('YTMP3_DB_NAME')
+
+if not AWS_DEFAULT_REGION:
+    raise Exception('AWS_DEFAULT_REGION undefined')
 
 if not YTMP3_STORE_BUCKET_NAME:
     raise Exception("YTMP3_STORE_BUCKET_NAME undefined")
@@ -37,6 +42,7 @@ def handler(sqs_event, context):
     logger.info(sqs_event)
     logger.info(context)
 
+    dyn_table = boto3.resource('dynamodb').Table(YTMP3_DB_NAME)
     sqs_client = boto3.client('sqs')
     jobs = sqs_event['Records']
 
@@ -82,14 +88,55 @@ def handler(sqs_event, context):
                 logger.info(f"Video ID: {video_id}, Duration: {duration}")
 
                 ydl.download([url])
-                upload_file(f"{file_path}/{file_name}",YTMP3_STORE_BUCKET_NAME, file_name)
+                upload_file(f"{file_path}/{file_name}",
+                            YTMP3_STORE_BUCKET_NAME, file_name)
                 logger.info(f"Finished downloading video {video_id}")
+
+                dyn_table.update_item(
+                    Key={
+                        'videoId': video_id,
+                    },
+                    AttributeUpdates={
+                        'updatedAt': {
+                            'Value': datetime.now().isoformat(),
+                            'Action': 'PUT'
+                        },
+                        'status': {
+                            'Value': 'COMPLETE',
+                            'Action': 'PUT'
+                        },
+                        'url': {
+                            'Value': f"{YTMP3_STORE_BUCKET_NAME}.s3.{AWS_DEFAULT_REGION}.amazonaws.com/{file_name}",
+                            'Action': 'PUT'
+                        }
+                    }
+                )
+
         except Exception as e:
             logger.error(f'Failed to process job {job_id}')
             logger.error(e)
             sqs_client.delete_message(
                 QueueUrl=YTMP3_DOWNLOADER_QUEUE_URL,
                 ReceiptHandle=job_reciept_handle
+            )
+            dyn_table.update_item(
+                Key={
+                    'videoId': video_id,
+                },
+                AttributeUpdates={
+                    'updatedAt': {
+                        'Value': datetime.now().isoformat(),
+                        'Action': 'PUT'
+                    },
+                    'status': {
+                        'Value': 'FAILED',
+                        'Action': 'PUT'
+                    },
+                    'error': {
+                        'Value': f'Failed to download {video_id}, please try again in 1 minute',
+                        'Action': 'PUT'
+                    }
+                }
             )
             return {
                 "statusCode": 500
