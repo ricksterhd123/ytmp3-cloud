abe61e5fc9bf539c912e4680ffd9958c59e8522d
ricksterhd123 <ricksterhd123@gmail.com>
2023-04-30 06:19:59 +0100

    Remove ECR in destroy script

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -2,7 +2,4 @@
 set -e
 
 STACK_NAME="ytmp3-cloud"
-YTMP3_DOWNLOADER_ECR_REPO_NAME="ytmp3-downloader"
-
 aws cloudformation delete-stack --stack-name $STACK_NAME
-aws ecr delete-repository --force --repository-name $YTMP3_DOWNLOADER_ECR_REPO_NAME
