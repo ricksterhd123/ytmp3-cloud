63a6456d8c56142bfad9132d6f27d2579cad77cd
ricksterhd123 <ricksterhd123@gmail.com>
2023-09-10 22:22:17 +0100

    Oops... I've leaked my bot token

--- ytmp3-discord-bot/config.py
+++ ytmp3-discord-bot/config.py
@@ -1,6 +1,6 @@
 config = {
     "command_prefix": "$",
     "log_path": "logs/ytmp3-discord-bot.log",
-    "bot_token": "MTE0NTg4NDk5NjI4NDk3MzA3Ng.G2V7kE.wPCKPTvXI7TwejY-JtAyA6Ah-1hMg5oNQ4odKg",
-    "ytmp3_api_url": "https://m1me7twesh.execute-api.us-east-1.amazonaws.com"
+    "bot_token": "",
+    "ytmp3_api_url": ""
 }
